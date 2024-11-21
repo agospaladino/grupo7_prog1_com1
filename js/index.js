fcd67dfe50b6f39f6502b4b2987c7d9c8353d9be
agospaladino <apaladino@udesa.edu.ar>
2024-11-21 14:34:00 -0300

    fetch de article en el index.js

--- js/index.js
+++ js/index.js
@@ -20,3 +20,39 @@ fetch("https://dummyjson.com/recipes")
             `;
         }
     })
+
+let button = document.querySelector("#home button");
+let pagina = 0;
+
+boton.addeventlistenner('click', function(){
+    console.log('tocaste el boton')
+    pagina += 10;
+    
+    fetch(`https://dummyjson.com/recipes?limit=10&skip=10&select=name,image=${pagina}`)
+    .then(function (res) {
+        return res.json()
+    })
+    .then(function (data) {
+        console.log(data)
+
+        for (let i = 0; i < data.recipes.length; i++) {
+            section.innerHTML +=
+            `
+             <article>
+                <img src="${data.recipes[i].image}" alt="">
+                <div>
+                    <h3>${data.recipes[i].name}</h3>
+                    <p>${data.recipes[i].difficulty}</p>
+                    <a href="detalle-receta.html?id=${data.recipes[i].id}">Ir a detalle</a>
+                </div>
+                
+            </article>
+            `
+        }
+        if(pagina == 50){
+            boton.innerHTML = "ya no hay mas elementos por mostrar"
+            boton.disabled = true
+        }
+    })
+}
+)
