303eb711b745e11ab637d87f3eadf9525d4fb06a
amaagomez <agomezdisanto@udesa.edu.ar>
2024-11-27 18:12:58 -0300

    JS login

--- js/login.js
+++ js/login.js
@@ -1,2 +1,25 @@
+// Capturo elementos
+let formLogIn = document.querySelector(".form-login");
+let email = document.querySelector("#email-login");
+let password = document.querySelector("#password-login");
+let boton = document.querySelector(".boton-login");
 
+formLogIn.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let valida = true;
+
+    if (email.value == "") {
+        alert("Por favor complete el campo email.");
+        valida = false;
+    }
+    if (password.value == "") {
+        alert("Por favor complete el campo contraseña.");
+        valida = false;
+    }
+    if (valida) {
+        boton.innerHTML = `<a href="./index.html"></a>`;
+        formLogIn.submit();
+    }
+})
 
