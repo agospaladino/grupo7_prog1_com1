75305c365ac65379a672229cc164c15b798306ab
agospaladino <apaladino@udesa.edu.ar>
2024-11-28 03:24:49 -0300

    main, h1 y section con sus id en search results

--- js/categories.js
+++ js/categories.js
@@ -1,10 +1,7 @@
-let queryString = location.search;
-let obj = new URLSearchParams(queryString);
-let categories = obj.get("categories");
-
 // Capturo elemento
 let categoriasContainer = document.querySelector(".categorias-container");
-let url = 'https://dummyjson.com/recipes/tags';
+
+let url = "https://dummyjson.com/recipes/tags";
 
 fetch(url)
     .then(function (response) {
@@ -13,6 +10,26 @@ fetch(url)
     .then(function (data) {
         console.log(data);
         
-        let datos = "";
-        
+        if (data.length > 0) {
+            let categorias = "";
+            for (let i = 0; i < data.length; i++) {
+                const categoria = data[i];
+
+                categorias += `
+                    <article class="categorias-article">
+                        <ul>
+                            <li><a href="category.html?category=${categoria}" class="categories-opciones">${categoria}</a></li>
+                        </ul>
+                    </article>`;
+                
+            }
+            categoriasContainer.innerHTML = categorias;
+        }else {
+            // Si 'tags' no existe o está vacío, mostramos mensaje:
+            categoriasContainer.innerHTML = "<p>No se encontraron categorías.</p>";
+        }
+    })
+    .catch(function (error) {
+        console.log("Error al cargar las categorías " + error);
+        categoriasContainer.innerHTML = "<p>Hubo un error al cargar las categorías.</p>";
     })
